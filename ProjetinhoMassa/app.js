7ce2d1d2897e0e06d8388d188a5f7dc3897d4ed2
Dokiemon <131017111+Dokiemon@users.noreply.github.com>
2025-04-11 13:06:27 +0000

    UDSSREGYWYETUERT

--- ProjetinhoMassa/app.js
+++ ProjetinhoMassa/app.js
@@ -5,6 +5,10 @@ app.get('', (req, res) => {
     res.send('hello world')
 })
 
-app.listen(3000, () => {
+app.get('/parental', (req, res) => {
+    res.send('MÃE COVARDE E PAI COVARDE')
+})
+const PORT= process.env.PORT || 3000
+app.listen(PORT, () => {
     console.log(`Example app listening on port `)
   })
